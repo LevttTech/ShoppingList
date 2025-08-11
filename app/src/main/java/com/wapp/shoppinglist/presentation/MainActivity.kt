94781d7fbf79dec4290e9ea21f39208e1803708e
Lev <levtokaevr@gmail.com>
2025-08-11 13:06:53 +0300

    create packages for clean arcрhitecture

--- app/src/main/java/com/wapp/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/wapp/shoppinglist/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.wapp.shoppinglist
+package com.wapp.shoppinglist.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.wapp.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
